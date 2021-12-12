69d28609af3b93095cd981bcec85fb333c3d197c
imharsha25 <96019335+imharsha25@users.noreply.github.com>
2021-12-12 23:24:26 +0530

    Add files via upload

---
+++ Fibonacci series.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def fib(n):
+    a=0
+    b=1
+    if n==1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+        for i in range(2,n):
+            c=a+b
+            a=b
+            b=c
+            print(c)
+fib(10)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
