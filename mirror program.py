c1745fb568f5ff0486b3513036c861c1fb0080bf
imharsha25 <96019335+imharsha25@users.noreply.github.com>
2021-12-12 23:47:22 +0530

    Add files via upload

---
+++ mirror program.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def reverse(str):
+    string = " "
+    for i in str:
+        string = i + string
+    return string
+str = "uma harsha"
+print("The original string is:",str)
+print("The reverse string is:", reverse(str)) 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
