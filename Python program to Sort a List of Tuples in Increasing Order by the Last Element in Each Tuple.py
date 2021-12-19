b7d0871c0d9357b68afbaff8ef38bc125cb5515b
imharsha25 <96019335+imharsha25@users.noreply.github.com>
2021-12-20 00:01:04 +0530

    Add files via upload

---
+++ Python program to Sort a List of Tuples in Increasing Order by the Last Element in Each Tuple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def last(n):
+    return n[-1]  
+   
+def sort(tuples):
+    return sorted(tuples, key=last)
+   
+a=[(1, 3), (3, 2), (2, 1)]
+print("Sorted:")
+print(sort(a))
+
+
+# In[ ]:
+
+
+
+
