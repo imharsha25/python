58d0b300d432a05ace21708896bacbf951e3545a
imharsha25 <96019335+imharsha25@users.noreply.github.com>
2022-01-12 00:37:29 +0530

    Add files via upload

---
+++ Calculate the Upper and The lower Case.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+string=input("Enter string:")
+count1=0
+count2=0
+for i in string:
+    if(i.islower()):
+        count1=count1+1
+    elif(i.isupper()):
+        count2=count2+1
+print("The number of lowercase characters is:")
+print(count1)
+print("The number of uppercase characters is:")
+print(count2)
+
+
+# In[ ]:
+
+
+
+
