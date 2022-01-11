2aa2fced3ed7e74c13d2a0d1e07c6cf2ee0763fa
imharsha25 <96019335+imharsha25@users.noreply.github.com>
2022-01-12 00:25:20 +0530

    Add files via upload

---
+++ Game of functions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+list1 = [11, 5, 17, 18, 23]
+total=0
+for i in range(0, len(list1)):
+    total += list1[i]
+print("Sum of all elements in given list: ", total)
+
+
+# In[ ]:
+
+
+
+
